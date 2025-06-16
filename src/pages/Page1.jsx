c7a43d044bcbf35b92d3212cca1ae60ee36085e0
Bhargav <bhargavahire2005@gmail.com>
2025-06-16 19:05:44 +0530

    text on page1

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import page1Text from '../components/Page1Text'
+import Page1Text from '../components/Page1Text'
 
 export const Page1 = () => {
   return (
-    <div className='h-screen  p-5 bg-white '> 
+    <div className='h-screen flex p-5 bg-white '> 
+
 
       <div className='  shadow-2xl shadow-gray-500 h-full w-full bg-black rounded-[20px] bg-[url(src/assets/images/bweye.jpeg)] bg-cover bg-right '> 
-      {/* og bg page which contain image */}
+        {/* og bg page which contain image */}
 
-      <div className='h-full w-full bg-black rounded-[20px] mask-r-to-80% hover:blur-xl'> 
-      {/* mask for image */}
 
-      </div>
+          {/* <img className='absolute z-10 h-32 w-40 ' src="src/assets/images/logo-removebg-preview.png" alt="" />
+           logo image */}
+
+          <Page1Text />
+           
+
+        <div className='h-full w-full bg-black rounded-[20px] mask-r-to-100% '> 
+          {/* mask for image */}
+
+
+        </div>
 
       </div>
 
