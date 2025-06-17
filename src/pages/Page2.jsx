e052df91b9ee28011b87f8b981b80ccc82ab2a4c
Your Name <your-email@example.com>
2025-06-17 22:04:25 +0530

    page2

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,67 +1,40 @@
-import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
+import React from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
 
 const Page2 = () => {
-  gsap.registerPlugin(ScrollTrigger)
-
-  
-  useGSAP(function () {
-    gsap.from(".rotateText", {
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(function(){
+    gsap.from('.rotateText',{
       transform: 'rotateX(-80deg)',
       opacity:0,
-      duration:1,
+      duration: 1,
       stagger: 1,
-      scrollTrigger: {
-        trigger: '.rotateText',
-        markers: true,
-        start:"top 60%" ,
-        end: "top -270%",
-        scrub: 2
+      scrollTrigger:{
+      trigger: '.rotateText',
+      //  markers: true,
+       start: 'top 50%',
+       end: 'top -100%',
+      scrub: 1
       }
     })
+
   })
   return (
-    <div id="section2" className="text-center text-black p-10 bg-white">
-      <h3 className="text-gray-500 text-2xl font-[anzo4]">
-        @ mantramuse.studio 2025 | create and design
-      </h3>
-      <div  className="rotateText bg-white mt-[60px]">
-        <h1  className="text-[40vw] text-black font-[anzo2] uppercase leading-[33vw]">
-          Creativity
-        </h1>
+    <div id='section2'className='text-center text-black bg-white pt-5 p-28'>
+      <h3 className='text-center text-grey-500 text-xl font-[anzo4]'>@ manatramuse.studio 2025 | design and create</h3>
+      <div className='rotateText  mt-5'>
+        <h1 className='text-[38vw] overflow-hidden font-[anzo2]  text-black uppercase leading-[33vw]'>Creativity</h1>
       </div>
-      <div className="rotateText bg-white">
-        <h1 className="text-[40vw] text-black font-[anzo2] uppercase leading-[33vw]">
-          Takes
-        </h1>
+      <div className='rotateText '>
+        <h1 className='text-[38vw] font-[anzo2]  overflow-hidden text-black uppercase leading-[33vw]'>takes</h1>
       </div>
-      <div className="rotateText bg-white">
-        <h1 className="text-[40vw] text-black font-[anzo2] uppercase leading-[33vw]">
-          Courage!
-        </h1>
+      <div className='rotateText '>
+        <h1 className='text-[38vw] font-[anzo2] overflow-hidden text-black uppercase leading-[33vw]'>courage!</h1>
       </div>
-      {/* <div className="rotateText bg-white">
-        <h1 className="text-[38vw] text-black font-[anzo2] uppercase leading-[31vw]">
-          and
-        </h1>
-      </div>
-      <div className="rotateText bg-white">
-        <h1 className="text-[38vw] text-black font-[anzo2] uppercase leading-[31vw]">
-          we
-        </h1>
-      </div>
-      <div className="rotateText bg-white">
-        <h1 className="text-[38vw] text-black font-[anzo2] uppercase leading-[31vw]">
-          are
-        </h1>
-      </div>
-      <div className="bg-white">
-        <h1 className="text-[38vw] text-black font-[anzo2] uppercase leading-[31vw]">
-          Courageous
-        </h1>
-      </div> */}
     </div>
   );
 };
