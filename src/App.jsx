211f9db650be3e36e390644ffb8b47a89f9113b5
Your Name <your-email@example.com>
2025-06-17 11:42:57 +0530

    Save my local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Page1  from './pages/Page1'
 import Header from './components/Header'
+import Page2  from './pages/Page2'
+
 
 const App = () => {
   return (
     <>
       <Header />
       <Page1 />
+      <Page2/>
     </>
   )
 }
